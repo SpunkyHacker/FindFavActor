d6ad41da5a59bccbc7b03621930243b36eb25c88
SpunkyHacker <tharunmuthu200@gmail.com>
2023-04-18 23:44:01 +0530

    90% done

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import random
 
 pygame.init()
+pygame.font.init()
+
+POINT_FONT = pygame.font.SysFont('comicsans', 40)
+
+WHITE = (255, 255, 255)
 
 FPS = 60
 
@@ -12,7 +17,7 @@ WIDTH = 500
 IMAGE_HEIGHT = 400
 IMAGE_WIDTH = 400
 
-
+point = 0
 
 window = pygame.display.set_mode((WIDTH,HEIGHT))
 
@@ -34,7 +39,7 @@ def chooseRandomImage(Correct,Preference):
         #rajinikanth
         if Preference == "Rajinikanth":
             Correct = True
-        addrs = randomLine("RanjiniKanth.txt")
+        addrs = randomLine("RajiniKanth.txt")
     elif rnum == 3:
         #Vijay
         if Preference == "Vijay":
@@ -44,18 +49,24 @@ def chooseRandomImage(Correct,Preference):
     return addrs,Correct
 
 
-def drawWindow(imageRect, AJITH_IMAGE):
+def drawWindow(imageRect, AJITH_IMAGE, point):
+
     window.blit(AJITH_IMAGE, (imageRect.x, imageRect.y))
 
+    pointText = POINT_FONT.render(str(point), 1, WHITE)
+    window.blit(pointText,(0,0))
     pygame.display.update()
 
 def main():
+    window.fill((0,0,0))
+    global point
 
     Correct = False
     Preference = "Vijay"
 
+
     addrs, Correct = chooseRandomImage(Correct,Preference)
-    AJITH_IMAGE = pygame.transform.scale(pygame.image.load(addrs), (IMAGE_WIDTH,IMAGE_HEIGHT))
+    IMAGE = pygame.transform.scale(pygame.image.load(addrs), (IMAGE_WIDTH,IMAGE_HEIGHT))
     
     imageRect = pygame.Rect(50,50, 400, 400)
 
@@ -67,8 +78,21 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE: #correct
+                    if Correct:
+                        point+=2
+                    else:
+                        point-=1
+                    main()
+                if event.key == pygame.K_LCTRL: #others
+                    if Correct:
+                        point-=5
+                    else:
+                        point+=1
+                    main()
 
-        drawWindow(imageRect, AJITH_IMAGE)      
+        drawWindow(imageRect, IMAGE, point)      
     print("done")
 
 
